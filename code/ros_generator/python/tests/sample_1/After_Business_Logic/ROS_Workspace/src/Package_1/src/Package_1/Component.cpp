b695470e856035e068be2875df2681e8d2db50ae
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2014-12-17 14:58:08 -0600

    Added workspace code after business logic addition

---
+++ code/ros_generator/python/tests/sample_1/After_Business_Logic/ROS_Workspace/src/Package_1/src/Package_1/Component.cpp
@@ -0,0 +1,21 @@
+#include "Package_1/Component.hpp"
+
+// required for clean shutdown when process is killed
+Component::~Component()
+{
+    compQueue.disable();
+    initOneShotTimer.stop();
+}
+
+void Component::Init(const ros::TimerEvent& event)
+{
+}
+
+void Component::processQueue()
+{  
+    ros::NodeHandle nh;
+    while (nh.ok())
+    {
+	this->compQueue.callAvailable(ros::WallDuration(0.01));
+    }
+}
