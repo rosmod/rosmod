86df334be9d49b9bd72b11141877ae542bf24c5e
pranav-srinivas-kumar <pranav.srinivas.kumar@gmail.com>
2014-12-26 00:00:38 -0600

    Added rosmod source skeleton! Builds fine with rest of ROS source.

---
+++ code/rosmod/include/rosmod/component_message_queue.h
@@ -0,0 +1,12 @@
+#ifndef COMPONENT_MESSAGE_QUEUE_H_
+#define COMPONENT_MESSAGE_QUEUE_H_
+
+#include "ros/ros.h"
+
+class Component_Message_Queue {
+
+
+
+};
+
+#endif
---
+++ code/rosmod/src/rosmod/component_message_queue.cpp
@@ -0,0 +1,1 @@
+#include "rosmod/component_message_queue.h"
