a7a61e289b6540654117dc39feae0231acc80975
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-07 11:07:04 -0600

    Added a tooltip example to provide info for buttons.  also trying to update terminal so that it resizes well.

--- code/rosmod_v2/editor_v2/unitTests/tabTerminal.py
+++ code/rosmod_v2/editor_v2/unitTests/tabTerminal.py
@@ -17,10 +17,14 @@ os.system('xterm -into %d &' % wid)
 
 Button(tab2, text='Exit', command=root.destroy).pack(padx=100, pady=100)
 
+tab1.pack(fill=BOTH,expand=1)
+tab2.pack(fill=BOTH,expand=1)
+tab3.pack(fill=BOTH,expand=1)
+
 note1.add(tab1, text = "Tab One", compound=TOP)
 note1.add(tab2, text = "Tab Two")
 note1.add(tab3, text = "Tab Three")
-note1.pack()
+note1.pack(fill=BOTH, expand=1)
 
 root.mainloop()
 exit()
