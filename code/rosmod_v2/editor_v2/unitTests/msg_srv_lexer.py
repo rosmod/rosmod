b5b885d0291dc284c48afa5fac4feb866d3f3484
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-07 13:42:07 -0600

    updated to have mostly working syntax highlighting for msgs and srv.

---
+++ code/rosmod_v2/editor_v2/unitTests/msg_srv_lexer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from pygments.lexer import RegexLexer, words, bygroups
+from pygments.token import *
+
+class MsgSrvLexer(RegexLexer):
+    name = 'MsgSrv'
+    aliases = ['msg','srv']
+    filenames = ['*.msg','*.srv']
+
+    tokens = {
+        'root' : [
+            (words((
+                'bool',
+                'int8',
+                'uint8',
+                'int16',
+                'uint16',
+                'int32',
+                'uint32',
+                'int64',
+                'uint64',
+                'float32',
+                'float64',
+                'string',
+                'time',
+                'duration')), Keyword),
+            (r'([ ]\S+)([ ]=[ ])(\S+)', bygroups(Text,Operator,String))
+        ]
+    }
--- code/rosmod_v2/editor_v2/unitTests/syntaxHighlighting.py
+++ code/rosmod_v2/editor_v2/unitTests/syntaxHighlighting.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 from Tkinter import *
 from ttk import *
-from pygments.lexers import HtmlLexer, PythonLexer
+from pygments.lexers import HtmlLexer, PythonLexer, CppLexer
 from tkformatter  import TkFormatter
 from pygtext import PygmentsText
+from msg_srv_lexer import MsgSrvLexer
 
 root = Tk()
 
 root.attributes('-topmost', 1)
 
-format = "python"
-
-py = open('syntaxHighlighting.py').read()
-ptext = PygmentsText(root,PythonLexer(), TkFormatter())
+py = open('test.msg').read()
+#ptext = PygmentsText(root,CppLexer(), TkFormatter())
+ptext = PygmentsText(root,MsgSrvLexer(), TkFormatter())
 ptext.insertFormatted("end",py)
 ptext.pack()
 root.mainloop()
