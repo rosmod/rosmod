14936b2954e2612a47f62d314329b5a32574d87f
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-07 15:29:31 -0600

    added initial version of popupwindow.

---
+++ code/rosmod_v2/editor_v2/unitTests/popupWindow.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+from Tkinter import * 
+import tkFileDialog
+import tkMessageBox
+from collections import OrderedDict
+
+class Dialog(Toplevel):
+
+    def __init__(self, parent, title = None, initValsList=None):
+
+        Toplevel.__init__(self, parent)
+        self.transient(parent)
+
+        self.valsList = initValsList
+
+        if title:
+            self.title(title)
+
+        self.parent = parent
+
+        self.entries = []
+        self.entryStrings = {}
+        self.options = []
+        self.stringVars = []
+        self.result = None
+
+        body = Frame(self)
+        self.initial_focus = self.body(body)
+        body.pack(padx=5, pady=5)
+
+        self.buttonbox()
+
+        self.grab_set()
+
+        if not self.initial_focus:
+            self.initial_focus = self
+
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+
+        self.geometry("+%d+%d" % (parent.winfo_rootx()+50,
+                                  parent.winfo_rooty()+50))
+
+        self.initial_focus.focus_set()
+
+        self.wait_window(self)
+
+    #
+    # construction hooks
+    def body(self, master):
+        pass
+
+    def buttonbox(self):
+        box = Frame(self)
+
+        w = Button(box, text="OK", width=10, command=self.ok, default=ACTIVE)
+        w.pack(side=LEFT, padx=5, pady=5)
+        w = Button(box, text="Cancel", width=10, command=self.cancel)
+        w.pack(side=LEFT, padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack()
+
+    #
+    # standard button semantics
+
+    def ok(self, event=None):
+
+        if not self.validate():
+            self.initial_focus.focus_set() # put focus back
+            return
+
+        self.withdraw()
+        self.update_idletasks()
+
+        self.apply()
+
+        self.cancel()
+
+    def cancel(self, event=None):
+        # put focus back to the parent window
+        self.parent.focus_set()
+        self.destroy()
+
+    #
+    # command hooks
+    def validate(self):
+        return 1 # override
+    def apply(self):
+        pass # override
