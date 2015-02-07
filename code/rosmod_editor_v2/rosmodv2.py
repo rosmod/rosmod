376dd975448b95d4212f939d202efdb0230c8544
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-06 21:23:34 -0600

    Updated comments to describe more of what's needed.

--- code/rosmod_editor_v2/rosmodv2.py
+++ code/rosmod_editor_v2/rosmodv2.py
@@ -43,6 +43,42 @@ import os
 # and they need to fill the frame of the window and resize properly when the main editor
 # is resized
 
+# these notebooks should be spawnable and killable by the window menubar calls
+
+# the tabs of the notebooks should be generated automatically for each aspect
+# * the packages aspect has a tab for each package and a tab containing all package info
+# * the hardware aspect only has one tab (for now)
+# * the deployment aspect has a tab for each package and a tab containing all package info
+
+# each aspect determines its own context menu (right click menu)
+
+# each object (drawn on an aspect canvas) determines its own context menu
+
+# each object (and context menu) can have it's own pop-ups (e.g. msg editors)
+# which must be managed and must take control to prevent the user from 
+# interacting with the main editor window while the popup is active (this 
+# was already present in v1 of the editor).  
+
+# these popups must support object references, text input, name input, etc.
+# and must be spawnable from context menus
+
+# A state change is only triggered by the main menubar of the application
+# i.e. modifying one of the aspects (insertion, modification, deletion) does
+# not ever switch aspects
+
+# Inside the deployment aspect, we can deploy nodes or applications to hosts
+# using a context menu, which controls a process pool that governs lifecycle of the
+# testbed.  This must be aware of what is currently happening in the system
+# and keep track of its own state transitions (such that an app can only be stopped if 
+# it is running, and an app cannot be spawned if it has already been spawned.) This can 
+# be partially accomplisehd by keeping as little state as possible and querying the system:
+# code has already been developed which can tell if a certain node is running on a certain
+# host, and can kill the node if it is, and start the node if it isn't
+
+# for deployment managing, we will probably need to have multiple subprocesses/threads 
+# which handle lifecycle management of the processes, getting logs, setting up nodes,
+# testing connectivity, and editor state update
+
 class rosmod_editor:
 
     def __init__(self, master, options = None):
