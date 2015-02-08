fffdf3eacb39617ccb45b6e81a84db6c3c27a192
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-08 00:29:36 -0600

    Updated tabs example to show simple button which closes the active tab.  Will be useful for deleting packages and closing console windows etc.

--- code/rosmod_v2/editor_v2/unitTests/tabs.py
+++ code/rosmod_v2/editor_v2/unitTests/tabs.py
@@ -6,6 +6,11 @@ root = Tk()
 
 note = Notebook(root)
 
+
+def closeTab():
+    activeTab = note.select()
+    note.forget(activeTab)
+
 tab1 = Frame(note)
 tab2 = Frame(note)
 tab3 = Frame(note)
@@ -14,6 +19,9 @@ Button(tab1, text='Exit', command=root.destroy).pack(padx=100, pady=100)
 note.add(tab1, text = "Tab One", compound=TOP)
 note.add(tab2, text = "Tab Two")
 note.add(tab3, text = "Tab Three")
+btn1 = Button(root, text="Close Active Tab", command = closeTab)
+btn1.pack()
 note.pack()
 root.mainloop()
 exit()
+
