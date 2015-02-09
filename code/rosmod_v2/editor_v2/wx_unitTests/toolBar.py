69ae8b24caefe1afcae91366a58a990f927443ef
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-08 19:12:12 -0600

    Added toolbar sample.

---
+++ code/rosmod_v2/editor_v2/wx_unitTests/toolBar.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# this code: http://zetcode.com/wxpython/menustoolbars/
+
+import wx
+
+class Example(wx.Frame):
+    
+    def __init__(self, *args, **kwargs):
+        super(Example, self).__init__(*args, **kwargs) 
+            
+        self.InitUI()
+        
+    def InitUI(self):    
+
+        toolbar = self.CreateToolBar()
+        qtool = toolbar.AddLabelTool(wx.ID_ANY, 'Quit', wx.Bitmap('textit.png'))
+        toolbar.Realize()
+
+        self.Bind(wx.EVT_TOOL, self.OnQuit, qtool)
+
+        self.SetSize((250, 200))
+        self.SetTitle('Simple toolbar')
+        self.Centre()
+        self.Show(True)
+        
+    def OnQuit(self, e):
+        self.Close()
+        
+def main():
+    
+    ex = wx.App()
+    Example(None)
+    ex.MainLoop()    
+
+
+if __name__ == '__main__':
+    main()
