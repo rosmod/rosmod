77b4d35d6c1e800a5ab799f0c37267bbfe4d3a3a
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-08 23:03:00 -0600

    Updated integration example: updated icon sizes to all be 25x25; updated status bar text for menu options; updated toolbar and status bar hiding code to work properly :)

--- code/rosmod_v2/editor_v2/wx_unitTests/integration.py
+++ code/rosmod_v2/editor_v2/wx_unitTests/integration.py
@@ -26,26 +26,30 @@ class Example(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnQuit, qmi)
 
         toolMenu = wx.Menu()
-        toolMenu.Append(wx.ID_ANY, "Generate ROS Code")
-        toolMenu.Append(wx.ID_ANY, "Analyze Network")
-        toolMenu.Append(wx.ID_ANY, "Analyze Timing")
+        toolMenu.Append(wx.ID_ANY, "Generate ROS Code", "Generate ROS application code and workspace.")
+        toolMenu.Append(wx.ID_ANY, "Analyze Network", "Analyze application and system network resource utilization.")
+        toolMenu.Append(wx.ID_ANY, "Analyze Timing", "Generate CPN Tokens and Analyze Business Logic Model.")
         
+        # useful for nested menus
         #fileMenu.AppendMenu(wx.ID_ANY, '&Tools', toolMenu)
 
         viewMenu = wx.Menu()
         self.shst = viewMenu.Append(wx.ID_ANY, 'Show Statusbar', 'Show Statusbar', kind=wx.ITEM_CHECK)
         self.shtl = viewMenu.Append(wx.ID_ANY, 'Show Toolbar', 'Show Toolbar', kind=wx.ITEM_CHECK)
-        #self.shvw = viewMenu.Append(wx.ID_ANY, 'Show Viewer', 'Show Viewer', kind=wx.ITEM_CHECK)
-        #self.shop = viewMenu.Append(wx.ID_ANY, 'Show Output', 'Show Output', kind=wx.ITEM_CHECK)
+        self.shvw = viewMenu.Append(wx.ID_ANY, 'Show Viewer', 'Show Viewer', kind=wx.ITEM_CHECK)
+        self.shop = viewMenu.Append(wx.ID_ANY, 'Show Output', 'Show Output', kind=wx.ITEM_CHECK)
 
         viewMenu.Check(self.shst.GetId(), True)
         viewMenu.Check(self.shtl.GetId(), True)
+        viewMenu.Check(self.shvw.GetId(), True)
+        viewMenu.Check(self.shop.GetId(), True)
 
         self.Bind(wx.EVT_MENU, self.ToggleStatusBar, self.shst)
         self.Bind(wx.EVT_MENU, self.ToggleToolBar, self.shtl)
 
         menubar.Append(fileMenu, '&File')
         menubar.Append(viewMenu, '&View')
+        menubar.Append(toolMenu, '&Tools')
         self.SetMenuBar(menubar)
 
         self.toolbar = self.CreateToolBar()
@@ -71,17 +75,12 @@ class Example(wx.Frame):
     def OnQuit(self, e):
         self.Close()
         
-    def ToggleStatusBar(self, e):        
-        if self.shst.IsChecked():
-            self.statusbar.Show()
-        else:
-            self.statusbar.Hide()
+    def ToggleStatusBar(self, e):
+        self.GetStatusBar().Show(e.IsChecked())
 
-    def ToggleToolBar(self, e):        
-        if self.shtl.IsChecked():
-            self.toolbar.Show()
-        else:
-            self.toolbar.Hide()  
+    def ToggleToolBar(self, e):
+        self.GetToolBar().Show(e.IsChecked())
+        self.SendSizeEvent()
 
 def main():
     
