5c16c246daea829b6379c67f3bcb161c8e7bcfee
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-09 18:15:37 -0600

    Updated aspect for better modularity and to fix some bugs

--- code/rosmod_v2/editor_v2/wx_unitTests/aspect.py
+++ code/rosmod_v2/editor_v2/wx_unitTests/aspect.py
@@ -10,19 +10,9 @@ class Aspect(wx.Panel):
     def __init__(self,parent,model=None):
         wx.Panel.__init__(self,parent)
 
-        self.toolbar = wx.ToolBar(self, -1)   
+        self.toolbar = wx.ToolBar(self)
         self.BuildToolbar()
-
-        self.notebook = wx.Notebook(self, id=wx.ID_ANY, style=
-                                     wx.BK_DEFAULT
-                                     #wx.BK_TOP 
-                                     #wx.BK_BOTTOM
-                                     #wx.BK_LEFT
-                                     #wx.BK_RIGHT
-                                 )
-        self.notebook.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.OnPageChanged)
-        self.notebook.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGING, self.OnPageChanging)
-        self.BuildPages()
+        self.BuildNotebook()
 
         self.boxSizer = wx.BoxSizer( wx.VERTICAL )     
         self.boxSizer.Add(self.toolbar, 0, wx.ALL | wx.ALIGN_LEFT | wx.EXPAND, 4)
@@ -40,11 +30,20 @@ class Aspect(wx.Panel):
         wx.EVT_TOOL( self, self.tcreate_ID, self.OnCreate)
         wx.EVT_TOOL( self, self.tdelete_ID, self.OnDelete)
 
-    def BuildPages(self):
+    def BuildNotebook(self):
+        self.notebook = wx.Notebook(self, id=wx.ID_ANY, style=
+                                     wx.BK_DEFAULT
+                                     #wx.BK_TOP 
+                                     #wx.BK_BOTTOM
+                                     #wx.BK_LEFT
+                                     #wx.BK_RIGHT
+                                 )
         self.notebook.AddPage(AspectTab(self.notebook), "Package 1")
         self.notebook.AddPage(AspectTab(self.notebook), "Package 2")
         self.notebook.AddPage(AspectTab(self.notebook), "Package 3")
         self.notebook.AddPage(AspectTab(self.notebook), "All Packages")
+        self.notebook.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.OnPageChanged)
+        self.notebook.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGING, self.OnPageChanging)
 
     def OnCreate(self, e):
         newTab = AspectTab(self.notebook)
@@ -68,7 +67,7 @@ class Aspect(wx.Panel):
             self.toolbar.EnableTool(self.tdelete.GetId(), False)
         else:
             self.toolbar.EnableTool(self.tdelete.GetId(), True)
-        print 'OnPageChanged,  old:%d, new:%d, sel:%d\n' % (old, new, sel)
+        #print 'OnPageChanged,  old:%d, new:%d, sel:%d\n' % (old, new, sel)
         event.Skip()
  
     def OnPageChanging(self, event):
@@ -80,5 +79,5 @@ class Aspect(wx.Panel):
             self.toolbar.EnableTool(self.tdelete.GetId(), False)
         else:
             self.toolbar.EnableTool(self.tdelete.GetId(), True)
-        print 'OnPageChanging, old:%d, new:%d, sel:%d\n' % (old, new, sel)
+        #print 'OnPageChanging, old:%d, new:%d, sel:%d\n' % (old, new, sel)
         event.Skip()
