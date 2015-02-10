070f1865a7a943efe258036a564d91ca10f49bef
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-02-10 16:16:26 -0600

    Updated setup script

--- code/rosmod_v2/setup.py
+++ code/rosmod_v2/setup.py
@@ -3,17 +3,18 @@ import os
 
 # Get setup path
 setup_path = os.getcwd()
-
+print "SETUP::Setting up Aliases.."
 # Create generator alias
 generator_path = os.path.join(setup_path, "generator_v2")
-generator_command = "echo alias rosgen=\'python " + os.path.join(generator_path, "rosgen.py") + "\'" + " >> ~/.bashrc"
-print generator_command
+generator_command = "echo \"alias rosgen='python " + os.path.join(generator_path, "rosgen.py") + "'\"" + " >> ~/.bashrc"
 os.system(generator_command)
+print "SETUP::Created Alias: rosgen = " + generator_path 
 
 # Create editor alias
 editor_path = os.path.join(setup_path, "editor_v2")
-editor_command = '''echo alias rosmod=\'python ''' + os.path.join(editor_path, "rosmod.py") + "\'"  + " >> ~/.bashrc"
-print editor_command
+editor_command = "echo \"alias rosmod='python " + os.path.join(editor_path, "rosmod.py") + "'\""  + " >> ~/.bashrc"
 os.system(editor_command)
+print "SETUP::Created Alias: rosmod = " + editor_path
+
 
 
