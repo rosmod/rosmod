5536150c2346840c94b00672cb586d2e2e037480
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-02-10 15:51:04 -0600

    Added a setup script to create aliases

---
+++ code/rosmod_v2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import os
+
+# Get setup path
+setup_path = os.getcwd()
+
+# Create generator alias
+generator_path = os.path.join(setup_path, "generator_v2")
+generator_command = "echo alias rosgen=\"python " + os.path.join(generator_path, "rosgen.py") + "\" >> ~/.bashrc"
+os.system(generator_command)
+
+# Create editor alias
+editor_path = os.path.join(setup_path, "editor_v2")
+editor_command = "echo alias rosmod=\"python " + os.path.join(editor_path, "rosmod.py") + "\" >> ~/.bashrc"
+os.system(editor_command)
+
+
