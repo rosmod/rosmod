a502b0ead0d19fbbdf41707c05b410039e10d7fa
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-02-10 18:06:18 -0600

    Removed commented line

--- code/rosmod_v2/setup.py
+++ code/rosmod_v2/setup.py
@@ -13,13 +13,13 @@ else:
     # Create generator alias
     generator_path = os.path.join(setup_path, "generator_v2")
     generator_command = "echo \"alias rosgen='python " + os.path.join(generator_path, "rosgen.py") + "'\"" + " >> ~/.bashrc"
-    # os.system(generator_command)
+    os.system(generator_command)
     print "SETUP::Created Alias: rosgen = " + generator_path 
 
     # Create editor alias
     editor_path = os.path.join(setup_path, "editor_v2")
     editor_command = "echo \"alias rosmod='python " + os.path.join(editor_path, "rosmod.py") + "'\""  + " >> ~/.bashrc"
-    # os.system(editor_command)
+    os.system(editor_command)
     print "SETUP::Created Alias: rosmod = " + editor_path
 
     # Setup ANTLR4
