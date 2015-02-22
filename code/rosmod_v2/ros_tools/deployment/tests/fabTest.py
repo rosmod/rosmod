4702ce8d788ece58c41e4e368988e3be205317c8
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-22 12:35:45 -0600

    Updated code to be able to talk to both the jetson and the BBB.  (I made the BBB @ olin have the jetson ssh key as an authorized key)

--- code/rosmod_v2/ros_tools/deployment/tests/fabTest.py
+++ code/rosmod_v2/ros_tools/deployment/tests/fabTest.py
@@ -2,11 +2,12 @@
 
 from fabric.api import env, run, sudo
 
-env.user = 'ubuntu'
 env.use_ssh_config = False
 env.key_filename = "~/.ssh/id_rsa_jetsontk1"
+env.hosts = ['ubuntu@129.59.79.182','ubuntu@129.59.79.66']
 def jetson_testfunc():
-    env.host_string = "ubuntu@129.59.79.182"
-    run('uname -a')
+    for host in env.hosts:
+        env.host_string = host
+        run('uname -a')
 
 jetson_testfunc()
