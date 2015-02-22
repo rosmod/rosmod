be89586dab94c1e3e0210344e42a634c862ba3c7
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-02-22 12:40:04 -0600

    Updated to fall more in line with the host definitions we'll use.

--- code/rosmod_v2/ros_tools/deployment/tests/fabTest.py
+++ code/rosmod_v2/ros_tools/deployment/tests/fabTest.py
@@ -3,11 +3,20 @@
 from fabric.api import env, run, sudo
 
 env.use_ssh_config = False
-env.key_filename = "~/.ssh/id_rsa_jetsontk1"
-env.hosts = ['ubuntu@129.59.79.182','ubuntu@129.59.79.66']
+
+class dep_node():
+    def __init__(self,userName,ipAddress,keyFile):
+        self.userName = userName
+        self.ipAddress = ipAddress
+        self.keyFile = keyFile
+
+hosts = [dep_node('ubuntu','129.59.79.182','~/.ssh/id_rsa_jetsontk1'),
+         dep_node('ubuntu','129.59.79.66','~/.ssh/id_rsa_jetsontk1')]
+
 def jetson_testfunc():
-    for host in env.hosts:
-        env.host_string = host
+    for host in hosts:
+        env.key_filename = host.keyFile
+        env.host_string = "{}@{}".format(host.userName,host.ipAddress)
         run('uname -a')
 
 jetson_testfunc()
