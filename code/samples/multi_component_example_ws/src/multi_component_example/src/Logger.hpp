0b2256e89729e7d9e099f9e3cf64276a54229285
William Emfinger <waemfinger@gmail.com>
2015-03-23 15:03:24 -0500

    Updated Logger to periodically write to file and NOT only when Destructor is called

--- code/samples/multi_component_example_ws/src/multi_component_example/src/Logger.hpp
+++ code/samples/multi_component_example_ws/src/multi_component_example/src/Logger.hpp
@@ -20,39 +20,43 @@ class Logger
 
 public:
 
-  // Default Constructor
-  Logger();
-
-  // Initializes Logger
-  Logger(const char * target_log_path);
-
   // Writes log_content to log file & empties log_content
   ~Logger();
 
   // Create log file
   bool CREATE_FILE(const char * target_log_path);
+
   // Write log contents to file
   bool WRITE_TO_FILE();
+
+  // Check size of log_contents
+  bool CHECK_LOG_SIZE();
+
   // Create a debug log entry
   bool DEBUG(string text);
+
   // Create an error log entry
   bool ERROR(string text);
 
+  // Get log entry size
+  int SIZE_OF_LOG();
+
   // Get Current Clock Value
-  string CLOCK_VALUE() {
-    // Clock Value String
-    stringstream clock_string;
-    clock_string << clock.now().time_since_epoch().count();
-    return clock_string.str();
-  }
+  string CLOCK_VALUE();
 
 private:
   // String representing the contents of log
   string log_content;
+
+  // Max size of log_content before contents are written to file
+  const int max_log_unit = 1000;
+
   // Log file stream
   ofstream log_stream;
+
   // Absolute path of log file
   const char * log_path;
+
   // High Resolution Clock
   chrono::high_resolution_clock clock;
 };
