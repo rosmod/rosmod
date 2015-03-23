8e0a08fa43ab5629bd84eb135dc896f1a45a4859
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-03-23 15:03:24 -0500

    Updated Logger to periodically write to file and NOT only when Destructor is called

--- code/samples/multi_component_example_ws/src/multi_component_example/src/Logger.cpp
+++ code/samples/multi_component_example_ws/src/multi_component_example/src/Logger.cpp
@@ -6,19 +6,20 @@
 
 #include "Logger.hpp"
 
-Logger::Logger(){}
-
-Logger::Logger(const char * target_log_path) {
-  log_content = "--------------------------------------------------------------------------------";
-  log_path = target_log_path;
-  log_stream.open(log_path, ios::out | ios::app );
-}
-
+/*
+ * Write remaining log contents to file
+ */
 Logger::~Logger() {
-  WRITE_TO_FILE();
-  log_content = "";
+  if (SIZE_OF_LOG() > 0) {
+    WRITE_TO_FILE();
+    log_content = "";
+  }
+  log_stream.close();
 }
 
+/*
+ * Open file outstream @ provided target path
+ */
 bool Logger::CREATE_FILE(const char * target_log_path) {
   log_path = target_log_path;
   log_stream.open(log_path, ios::out | ios::app );  
@@ -26,19 +27,63 @@ bool Logger::CREATE_FILE(const char * target_log_path) {
   return true;
 }
 
+/*
+ * Write Log contents to file & close stream
+ */ 
 bool Logger::WRITE_TO_FILE() {
   log_stream << log_content << endl;
-  log_stream.close();
   return true;
 }
 
+/*
+ * Check log content size 
+ * If size > 1000, write to file
+ */
+bool Logger::CHECK_LOG_SIZE() {
+  if (SIZE_OF_LOG() > max_log_unit) {
+    WRITE_TO_FILE();
+    log_content = "";
+    return true;
+  }
+  return false;
+}
+
+/*
+ * Create a debug log entry
+ */
 bool Logger::DEBUG(string text) {
-  log_content += "\nROSMOD::DEBUG::" + CLOCK_VALUE() + "::" + text; 
+  bool exceeded_limit = CHECK_LOG_SIZE();
+  if (exceeded_limit == true)
+    log_content += "ROSMOD::DEBUG::" + CLOCK_VALUE() + "::" + text;
+  else
+    log_content += "\nROSMOD::DEBUG::" + CLOCK_VALUE() + "::" + text;
   return true;
 }
 
+/*
+ * Create an error log entry
+ */
 bool Logger::ERROR(string text) {
-  log_content += "\nROSMOD::ERROR::" + CLOCK_VALUE() + "::" + text;
+  bool exceeded_limit = CHECK_LOG_SIZE();
+  if (exceeded_limit == true)
+    log_content += "ROSMOD::ERROR::" + CLOCK_VALUE() + "::" + text;
+  else
+    log_content += "\nROSMOD::ERROR::" + CLOCK_VALUE() + "::" + text;
   return true;
 }
 
+/*
+ * Return size of log_content
+ */
+int Logger::SIZE_OF_LOG() {
+  return log_content.size();
+}
+
+/*
+ * Get Current Clock Value
+ */
+string Logger::CLOCK_VALUE() {
+  stringstream clock_string;
+  clock_string << clock.now().time_since_epoch().count();
+  return clock_string.str();  
+}
