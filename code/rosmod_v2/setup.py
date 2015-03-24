4381f049289984210c2c35a537b02e483ed647c9
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-03-24 15:09:10 -0500

    Removed aliases from setup script

--- code/rosmod_v2/setup.py
+++ code/rosmod_v2/setup.py
@@ -10,17 +10,8 @@ else:
     # Get setup path
     setup_path = os.getcwd()
 
-    # Create generator alias
     generator_path = os.path.join(setup_path, "ros_tools")
-    generator_command = "echo \"alias rosgen='python " + os.path.join(generator_path, "rosgen.py") + "'\"" + " >> ~/.bashrc"
-    os.system(generator_command)
-    print "SETUP::Created Alias: rosgen = " + generator_path 
-
-    # Create editor alias
     editor_path = os.path.join(setup_path, "editor_v2")
-    editor_command = "echo \"alias rosmod='python " + os.path.join(editor_path, "rosmod.py") + "'\""  + " >> ~/.bashrc"
-    os.system(editor_command)
-    print "SETUP::Created Alias: rosmod = " + editor_path
 
     # Setup ANTLR4
     print "SETUP::Setting up Antlr4"
