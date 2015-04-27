dc594cd251f84125fa9641a8f2e312a66428e3c1
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-04-27 15:46:00 -0500

    Updated code to work for relevant sample

--- code/rosmod_v3/tests/parserClassFactoryTest.py
+++ code/rosmod_v3/tests/parserClassFactoryTest.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 class propDef:
     def __init__(self, 
                  kind = "",
@@ -15,47 +14,59 @@ class propDef:
         self.grammarExitFunc = grammarExitFunc
         self.intputValidator = inputValidator
 
-def enterPropFunc(self,*args,**kwargs):
-    print "ENTERING PROP FUNCTION"
-    print args
-    print kwargs
-def exitPropFunc(self,*args,**kwargs):
-    print "EXITING PROP FUNCTION"
-    print args
-    print kwargs
-def enterContainerFunc(self,*args,**kwargs):
-    print "ENTERING CONTAINER FUNCTION"
-    print args
-    print kwargs
-def exitContainerFunc(self,ctx):
-    print "EXITING CONTAINER FUNCTION"
-    print ctx
+
+
+def makeEnterProp(kind):
+    def enterPropFunc(self,ctx):
+        print "PROP::ENTER::" + ctx
+        print kind
+    return enterPropFunc
+
+def makeExitProp():
+    def exitPropFunc(self,ctx):
+        print "PROP::EXIT::" + ctx
+    return exitPropFunc
+
+def makeEnterContainer(kind):
+    def enterContainerFunc(self,ctx):
+        print "CONTAINER::ENTER::" + ctx
+        print kind
+    return enterContainerFunc
+
+def makeExitContainer():
+    def exitContainerFunc(self,ctx):
+        print "CONTAINER::EXIT::" + ctx
+    return exitContainerFunc
 
 objDefDict = {}
-objDefDict['name'] = propDef("string","Name",enterPropFunc,exitPropFunc)
-objDefDict['package'] = propDef("string","Package",enterContainerFunc,exitContainerFunc)
-objDefDict['message'] = propDef("string","Message",enterContainerFunc,exitContainerFunc)
-objDefDict['field'] = propDef("string","Field",enterContainerFunc,exitContainerFunc)
-objDefDict['type'] = propDef("string","Type",enterPropFunc,exitPropFunc)
-objDefDict['value'] = propDef("string","Value",enterPropFunc,exitPropFunc)
+objDefDict['name'] = propDef("string","Name",makeEnterProp,makeExitProp)
+objDefDict['type'] = propDef("string","Type",makeEnterProp,makeExitProp)
+objDefDict['value'] = propDef("string","Value",makeEnterProp,makeExitProp)
+objDefDict['package'] = propDef("obj","Package",makeEnterContainer,makeExitContainer)
+objDefDict['message'] = propDef("obj","Message",makeEnterContainer,makeExitContainer)
+objDefDict['field'] = propDef("obj","Field",makeEnterContainer,makeExitContainer)
 
 class myMetaClass(type):
     def __new__(cls, name, bases, attrs):
         if name.startswith('None'):
             return None
         for name,value in objDefDict.iteritems():
-            attrs['enter'+value.grammarName] = value.grammarEnterFunc
-            attrs['exit'+value.grammarName] = value.grammarExitFunc
+            attrs['enter'+value.grammarName] = value.grammarEnterFunc(name)
+            attrs['exit'+value.grammarName] = value.grammarExitFunc()
         return super(myMetaClass,cls).__new__(cls,name,bases,attrs)
     def __init__(self, name, bases, attrs):
         super(myMetaClass, self).__init__(name, bases, attrs)
 
 class myObj:
-    __metaclass__ = myMetaClass
 
-print type(myObj), repr(myObj)
+    __metaclass__ = myMetaClass
+    def __init__(self):
+        self.activeObjectList = []
+
 testObj = myObj()
-testObj.enterName()
-testObj.exitName()
-testObj.enterMessage("hi pranav")
-testObj.exitField(ctx="HELLO WORLD")
+testObj.enterName("prop")
+testObj.exitName("prop")
+testObj.enterMessage("container")
+testObj.exitMessage("container")
+testObj.enterField(ctx="HELLO WORLD")
+testObj.exitField(ctx="GOODBYE WORLD")
