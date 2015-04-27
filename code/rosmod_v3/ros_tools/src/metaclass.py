cf34739f2c5f2d32e06bf4a74a39dcb1ce16dd73
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-04-27 15:24:48 -0500

    Changed naming convention

--- code/rosmod_v3/ros_tools/src/metaclass.py
+++ code/rosmod_v3/ros_tools/src/metaclass.py
@@ -4,52 +4,52 @@
 
 from collections import OrderedDict
 
-# Grammar Property class used to generate grammar field-specific listener functions
-class Grammar_Property:
-    def __init__(self, kind = "", name = "", entry_point = None, exit_point = None, checker = None):
+# Grammar Field class used to generate grammar field-specific listener functions
+class Grammar_Field:
+    def __init__(self, kind = "", name = "", entry_point = None, exit_point = None, syntax_checker = None):
         self.kind = kind
         self.name = name
         self.entry_point = entry_point
         self.exit_point = exit_point
-        self.checker = checker
+        self.syntax_checker = syntax_checker
 
-# Enter a Container class type while parsing
+# Enter a Model class type while parsing
 # E.g. Package, Component, Timer etc.
-def enterContainer(self, ctx, kind):
-    print "enterContainerFunc:: Kind = " + kind
-    print "enterContainerFunc:: Parsed Text = " + ctx.getText()
+def enterModel(self, ctx, kind):
+    print "enterModel:: Kind = " + kind
+    print "enterModel:: Parsed Text = " + ctx.getText()
     new_object = type( "ROS_" + kind, (object, Drawable_Object,), { })
     self.active_objects.append(new_object)
     print "Active Objects: " + self.active_objects
     print type(new_object)
 
-# Exit a Container class type while parsing
-def exitContainer(self, ctx):
+# Exit a Model class type while parsing
+def exitModel(self, ctx):
     child_object = self.active_objects.pop()
     self.active_objects[-1].add(child_object)
 
-# Enter a Property type while parsing
+# Enter a Atom type while parsing
 # E.g. Name, Value, Period, Unit etc.
-def enterProperty(self, ctx, kind):
+def enterAtom(self, ctx, kind):
     print "enterPropFunc:: Kind = " + kind
     print "enterPropFunc:: Parsed Text = " + ctx.getText()
     self.active_objects[-1].properties[kind] = ctx.getText()
 
-# Exit a Property type while parsing
-def exitProperty(self, ctx):
+# Exit a Atom type while parsing
+def exitAtom(self, ctx):
     pass
 
-# Setup a Dictionary of Grammar fields that are recognized and visited while parsing input models
-# Use this dictionary to generate the listener functions inside the Builder classes
+# Setup a Dictionary of recognized & parsed grammar fields
+# Use this dictionary to generate the listener functions per field inside the builder classes
 meta_class_dict = OrderedDict()
-meta_class_dict["Package"] = Grammar_Property("string", "Package", enterContainer, exitContainer)
-meta_class_dict["name"] = Grammar_Property("string", "Name", enterProperty, exitProperty)
-meta_class_dict["value"] = Grammar_Property("string", "Value", enterProperty, exitProperty)
-meta_class_dict["unit"] = Grammar_Property("string", "Unit", enterProperty, exitProperty)
-meta_class_dict["datatype"] = Grammar_Property("string", "Datatype", enterProperty, exitProperty)
-meta_class_dict["Component"] = Grammar_Property("string", "Component", enterContainer, exitContainer)
-meta_class_dict["Timer"] = Grammar_Property("string", "Timer", enterContainer, exitContainer)
-meta_class_dict["period"] = Grammar_Property("string", "Period", enterProperty, exitProperty)
+meta_class_dict["Package"] = Grammar_Field("string", "Package", enterModel, exitModel)
+meta_class_dict["name"] = Grammar_Field("string", "Name", enterAtom, exitAtom)
+meta_class_dict["value"] = Grammar_Field("string", "Value", enterAtom, exitAtom)
+meta_class_dict["unit"] = Grammar_Field("string", "Unit", enterAtom, exitAtom)
+meta_class_dict["datatype"] = Grammar_Field("string", "Datatype", enterAtom, exitAtom)
+meta_class_dict["Component"] = Grammar_Field("string", "Component", enterModel, exitModel)
+meta_class_dict["Timer"] = Grammar_Field("string", "Timer", enterModel, exitModel)
+meta_class_dict["period"] = Grammar_Field("string", "Period", enterAtom, exitAtom)
 
 # Grammar Metaclass to generate listener functions as part of the builder classes
 class Grammar_MetaClass(type):
