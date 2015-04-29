b27addcee491468ca50cec6e8a7726a2ff3c7abe
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-04-28 20:45:55 -0500

    Working on representing meta-model as dictionary for generalization of editor logic.

--- code/rosmod_v3/src/metaclass.py
+++ code/rosmod_v3/src/metaclass.py
@@ -14,6 +14,15 @@ class Grammar_Field:
         self.exit_point = exit_point
         self.syntax_checker = syntax_checker
 
+# 
+class Meta_Entity:
+    def __init__(self, parent = "", children = [], properties = [], in_refs = [], out_refs = []):
+        self.parent = parent
+        self.children = children
+        self.properties = properties
+        self.in_refs = in_refs
+        self.out_refs = out_refs
+
 reference_dict = OrderedDict()
 
 # Enter a Model class type while parsing
@@ -52,33 +61,177 @@ def create_exitAtom():
         pass
     return exitAtom
 
+# Set up a dictionary which desribes the meta-model
+# for each entity in the metamodel:
+#  the types of its children
+#  the type of its parent
+#  the properties it has
+#  the types of objects which can refer to it
+#  the types of objects to which it can refer
+model_dict = OrderedDict()
+
+model_dict["rml"] = Meta_Entity(
+    "Project",
+    ["Package"],
+    ["name"],
+    [],
+    [])
+
+model_dict["Package"] = Meta_Entity(
+    "rml", 
+    ["Message","Service","Component","Node"],
+    ["name"],
+    [], 
+    [])
+
+model_dict["Message"] = Meta_Entity(
+    "Package", 
+    ["Field"], 
+    ["name"],
+    ["Publisher","Subscriber"], 
+    [])
+model_dict["Field"] = Meta_Entity(
+    "Message", 
+    [], 
+    ["datatype","name","value"],
+    [], 
+    [])
+
+model_dict["Service"] = Meta_Entity(
+    "Package", 
+    ["Request","Response"],
+    ["name"],
+    ["Server","Client"], 
+    [])
+model_dict["Request"] = Meta_Entity(
+    "Service",
+    [],
+    ["datatype","name","value"],
+    [], 
+    [])
+model_dict["Response"] = Meta_Entity(
+    "Service", 
+    [],
+    ["datatype","name","value"],
+    [], 
+    [])
+
+model_dict["Component"] = Meta_Entity(
+    "Package", 
+    ["Timer","Publisher","Subscriber","Client","Server"], 
+    ["name"],
+    ["Component_Instance"], 
+    [])
+model_dict["Client"] = Meta_Entity(
+    "Component", 
+    [], 
+    ["name","reference"],
+    ["Port_Instance"], 
+    ["Service"])
+model_dict["Server"] = Meta_Entity(
+    "Component", 
+    [],
+    ["name","reference","priority","deadline"],
+    ["Port_Instance"], 
+    ["Service"])
+model_dict["Publisher"] = Meta_Entity(
+    "Component", 
+    [], 
+    ["name","reference"],
+    ["Port_Instance"], 
+    ["Message"])
+model_dict["Subscriber"] = Meta_Entity(
+    "Component", 
+    [],
+    ["name","reference","priority","deadline"],
+    ["Port_Instance"], 
+    ["Message"])
+model_dict["Timer"] = Meta_Entity(
+    "Component", 
+    [],
+    ["name","period","priority","deadline"],
+    [], 
+    [])
+
+model_dict["Node"] = Meta_Entity(
+    "Package", 
+    ["Component_Instance"], 
+    ["name"],
+    ["Node_Instance"], 
+    [])
+model_dict["Component_Instance"] = Meta_Entity(
+    "Node", 
+    [], 
+    ["name","reference"],
+    [], 
+    ["Component"])
+
+model_dict["rhw"] = Meta_Entity(
+    "Project", 
+    "Hardware", 
+    ["rdp"], 
+    [])
+model_dict["Hardware"] = Meta_Entity(
+    "Project", 
+    "Hardware", 
+    ["Hardware_Instance"], 
+    [])
+
+model_dict["rdp"] = Meta_Entity(
+    "Project", 
+    ["Hardware_Instance"], 
+    ["name","rdp_hardware"],
+    [], 
+    ["rhw"])
+model_dict["Hardware_Instance"] = Meta_Entity(
+    "rhw", 
+    ["Node_Instance"],
+    ["name","ip_address","username","sshkey","init"], 
+    [], 
+    ["Hardware"])
+model_dict["Node_Instance"] = Meta_Entity(
+    "Hardware_Instance", 
+    ["Port_Instance"], 
+    ["name","reference","cmd_args"],
+    [], 
+    ["Node"])
+model_dict["Port_Instance"] = Meta_Entity(
+    "Node_Instance", 
+    [], 
+    ["name","reference","group"],
+    [], 
+    ["Client","Server","Publisher","Subscriber","Timer"])
+
+# Properties that are references
+model_dict["component_instance_reference"] = Meta_Entity("reference")
+
 # Setup a Dictionary of recognized & parsed grammar fields
 # Use this dictionary to generate the listener functions per field inside the builder classes
 meta_class_dict = OrderedDict()
 
 # Containers/Models
-meta_class_dict["Package"] = Grammar_Field("string", "Package", create_enterModel, create_exitModel)
+meta_class_dict["Package"] = Grammar_Field("object", "Package", create_enterModel, create_exitModel)
 
-meta_class_dict["Message"] = Grammar_Field("string", "Message", create_enterModel, create_exitModel)
-meta_class_dict["Field"] = Grammar_Field("string", "Field", create_enterModel, create_exitModel)
+meta_class_dict["Message"] = Grammar_Field("object", "Message", create_enterModel, create_exitModel)
+meta_class_dict["Field"] = Grammar_Field("object", "Field", create_enterModel, create_exitModel)
 
-meta_class_dict["Service"] = Grammar_Field("string", "Service", create_enterModel, create_exitModel)
-meta_class_dict["Request"] = Grammar_Field("string", "Request", create_enterModel, create_exitModel)
-meta_class_dict["Response"] = Grammar_Field("string", "Response", create_enterModel, create_exitModel)
+meta_class_dict["Service"] = Grammar_Field("object", "Service", create_enterModel, create_exitModel)
+meta_class_dict["Request"] = Grammar_Field("object", "Request", create_enterModel, create_exitModel)
+meta_class_dict["Response"] = Grammar_Field("object", "Response", create_enterModel, create_exitModel)
 
-meta_class_dict["Component"] = Grammar_Field("string", "Component", create_enterModel, create_exitModel)
-meta_class_dict["Client"] = Grammar_Field("string", "Client", create_enterModel, create_exitModel)
-meta_class_dict["Server"] = Grammar_Field("string", "Server", create_enterModel, create_exitModel)
-meta_class_dict["Publisher"] = Grammar_Field("string", "Publisher", create_enterModel, create_exitModel)
-meta_class_dict["Subscriber"] = Grammar_Field("string", "Subscriber", create_enterModel, create_exitModel)
+meta_class_dict["Component"] = Grammar_Field("object", "Component", create_enterModel, create_exitModel)
+meta_class_dict["Client"] = Grammar_Field("object", "Client", create_enterModel, create_exitModel)
+meta_class_dict["Server"] = Grammar_Field("object", "Server", create_enterModel, create_exitModel)
+meta_class_dict["Publisher"] = Grammar_Field("object", "Publisher", create_enterModel, create_exitModel)
+meta_class_dict["Subscriber"] = Grammar_Field("object", "Subscriber", create_enterModel, create_exitModel)
 
-meta_class_dict["Node"] = Grammar_Field("string", "Node", create_enterModel, create_exitModel)
-meta_class_dict["Component_Instance"] = Grammar_Field("string", "Component_instance", create_enterModel, create_exitModel)
-meta_class_dict["Timer"] = Grammar_Field("string", "Timer", create_enterModel, create_exitModel)
-meta_class_dict["Hardware"] = Grammar_Field("string", "Hardware", create_enterModel, create_exitModel)
-meta_class_dict["Hardware_Instance"] = Grammar_Field("string", "Hardware_instance", create_enterModel, create_exitModel)
-meta_class_dict["Node_Instance"] = Grammar_Field("string", "Node_instance", create_enterModel, create_exitModel)
-meta_class_dict["Port_Instance"] = Grammar_Field("string", "Port_instance", create_enterModel, create_exitModel)
+meta_class_dict["Node"] = Grammar_Field("object", "Node", create_enterModel, create_exitModel)
+meta_class_dict["Component_Instance"] = Grammar_Field("object", "Component_instance", create_enterModel, create_exitModel)
+meta_class_dict["Timer"] = Grammar_Field("object", "Timer", create_enterModel, create_exitModel)
+meta_class_dict["Hardware"] = Grammar_Field("object", "Hardware", create_enterModel, create_exitModel)
+meta_class_dict["Hardware_Instance"] = Grammar_Field("object", "Hardware_instance", create_enterModel, create_exitModel)
+meta_class_dict["Node_Instance"] = Grammar_Field("object", "Node_instance", create_enterModel, create_exitModel)
+meta_class_dict["Port_Instance"] = Grammar_Field("object", "Port_instance", create_enterModel, create_exitModel)
 
 # Properties/Atoms
 meta_class_dict["name"] = Grammar_Field("string", "Name", create_enterAtom, create_exitAtom)
