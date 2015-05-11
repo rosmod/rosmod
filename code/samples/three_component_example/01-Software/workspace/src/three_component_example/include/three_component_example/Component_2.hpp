b2422474981799eb13b9e1f31916a3d4e57975ba
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-05-11 10:57:43 -0500

    Added three_component_example. Porting business logic now

---
+++ code/samples/three_component_example/01-Software/workspace/src/three_component_example/include/three_component_example/Component_2.hpp
@@ -0,0 +1,53 @@
+#ifndef COMPONENT_2_HPP
+#define COMPONENT_2_HPP
+
+#include "ros/ros.h"
+#include "Component.hpp"
+#include "three_component_example/ComponentName.h"
+#include "three_component_example/ComponentService.h"
+//# Start User Includes Marker
+//# End User Includes Marker
+
+//# Start User Globals Marker
+//# End User Globals Marker
+
+class Component_2 : public Component
+{
+public:
+  // Constructor
+  Component_2(ComponentConfig& config, int argc, char **argv) : Component(config, argc, argv) {}
+
+  // Initialization
+  void Init(const ros::TimerEvent& event);
+
+  // Server Callback - Service_Server
+  bool ComponentServiceCallback(three_component_example::ComponentService::Request &req, 
+    three_component_example::ComponentService::Response &res);
+
+  // Timer Callback - Timer_2
+  void Timer_2Callback(const ros::TimerEvent& event);
+
+  // Start up
+  void startUp();
+
+  // Destructor
+  ~Component_2();
+
+private:
+
+  // Timer
+  ros::Timer Timer_2;
+
+  // Publisher 
+  ros::Publisher Name_Publisher;
+
+  // Server 
+  ros::ServiceServer Service_Server;
+
+  //# Start User Private Variables Marker
+    
+  //# End User Private Variables Marker
+};
+
+
+#endif
---
+++ code/samples/three_component_example/01-Software/workspace/src/three_component_example/src/three_component_example/Component_3.cpp
@@ -0,0 +1,92 @@
+#include "three_component_example/Component_3.hpp"
+
+//# Start User Globals Marker
+//# End User Globals Marker
+
+// Initialization Function
+//# Start Init Marker
+void Component_3::Init(const ros::TimerEvent& event)
+{
+  // Initialize Here
+
+  // Stop Init Timer
+  initOneShotTimer.stop();
+}
+//# End Init Marker
+
+// Timer Callback - Timer_3
+//# Start Timer_3Callback Marker
+void Component_3::Timer_3Callback(const ros::TimerEvent& event)
+{
+  // Business Logic for Timer_3 Timer
+}
+//# End Timer_3Callback Marker
+
+
+// Destructor - Cleanup Ports & Timers
+Component_3::~Component_3()
+{
+  Timer_3.stop();
+  ComponentService_client.shutdown();
+  //# Start Destructor Marker
+  
+  //# End Destructor Marker
+}
+
+// Startup - Setup Component Ports & Timers
+void Component_3::startUp()
+{
+  ros::NodeHandle nh;
+  std::string advertiseName;
+
+  // Configure all required services associated with this component
+  // Component Client - Service_Client
+  advertiseName = "ComponentService";
+  if (portGroupMap.find("Service_Client") != portGroupMap.end())
+    advertiseName += "_" + portGroupMap["client.properties["name"]"]
+      this->Service_Client = nh.serviceClient<three_component_example::ComponentService>(advertiseName.c_str()); 
+
+  // Init Timer
+  ros::TimerOptions timer_options;
+  timer_options = 
+    ros::TimerOptions
+    (ros::Duration(-1),
+     boost::bind(&Component_3::Init, this, _1),
+     &this->compQueue,
+     true);
+  this->initOneShotTimer = nh.createTimer(timer_options);  
+  
+  // Component Timer - timer.properties["name"]
+  timer_options = 
+    ros::TimerOptions
+    (ros::Duration(2.0),
+     boost::bind(&Component_3::Timer_3Callback, this, _1),
+     &this->compQueue);
+  this->Timer_3 = nh.createTimer(timer_options);
+
+  // Identify the pwd of Node Executable
+  std::string s = node_argv[0];
+  std::string exec_path = s;
+  std::string delimiter = "/";
+  std::string exec, pwd;
+  size_t pos = 0;
+  while ((pos = s.find(delimiter)) != std::string::npos) {
+    exec = s.substr(0, pos);
+    s.erase(0, pos + delimiter.length());
+  }
+  exec = s.substr(0, pos);
+  pwd = exec_path.erase(exec_path.find(exec), exec.length());
+  std::string log_file_path = pwd + nodeName + "." + compName + ".log"; 
+  
+  // Create the log file & open file stream
+  LOGGER.CREATE_FILE(log_file_path);
+  
+  // Establish log levels of LOGGER
+  LOGGER.SET_LOG_LEVELS(logLevels);
+}
+
+extern "C" {
+  Component *maker(ComponentConfig &config, int argc, char **argv) {
+    return new Component_3(config,argc,argv);
+  }
+}
