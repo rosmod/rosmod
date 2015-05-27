e0e6945723ee105832130c111d5c40e013ad49b5
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-05-27 13:58:41 -0500

    Added commandline parsing for no gui option

--- src/rosmod_v0.3/rosmod.py
+++ src/rosmod_v0.3/rosmod.py
@@ -891,13 +891,23 @@ class Example(wx.Frame):
         self.statusbar = self.CreateStatusBar()
         self.statusbar.SetStatusText('Ready')
 
-def main():
-    if 'DISPLAY' in os.environ.keys():
-        ex = wx.App()
-        Example(None)
-        ex.MainLoop()    
+def main(argv):
+    no_gui = False
+    for arg in argv:
+        if arg == "-ng" or arg == "--no-gui" or arg == "-nogui" or arg == "--nogui":
+            no_gui = True
+
+    # Is ROSMOD invoked with "no gui" option?
+    if no_gui == True:
+        print "ROSMOD Command Line Version!"
     else:
-        print "No Display Found! We don't yet have a purely command line version"
+        # If GUI required, check if DISPLAY exists
+        if 'DISPLAY' in os.environ.keys():
+            ex = wx.App()
+            Example(None)
+            ex.MainLoop()    
+        else:
+            print "ERROR::No Display Found! Please run ROSMOD with no GUI option \"-ng\""
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
