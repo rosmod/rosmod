342eb62b0b2dfda5349f4b75250063c6e7f1fb92
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-06-26 13:48:59 -0500

    Added a create_egg bash script that will create rosmod.egg and move to dest. directory

--- src/rosmod_v0.3/create_egg.bash
+++ src/rosmod_v0.3/create_egg.bash
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-zip -r ../rosmod.zip *
-mv ../rosmod.zip ../rosmod.egg
+if [ $1 = ""]; then
+    echo "Usage: ./create_egg.bash <path_to_save_egg>"
+    echo "Example: ./create_egg.bash /home/jeb"
+else
+    zip -r ../rosmod.zip *
+    mv ../rosmod.zip ../rosmod.egg
+    mv ../rosmod.egg $1
+    echo "Created rosmod.egg at path:" $1
+fi
