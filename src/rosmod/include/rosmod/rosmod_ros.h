db7961d1054d57f33fb3740cfe3e6003b1cbbe14
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-07-13 10:47:24 -0500

    Added prototype implementaion of ROSMOD runtime

---
+++ src/rosmod/include/rosmod/rosmod_ros.h
@@ -0,0 +1,58 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2009, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef ROSMOD_ROS_H
+#define ROSMOD_ROS_H
+
+#include "ros/time.h"
+#include "ros/rate.h"
+#include "ros/console.h"
+#include "ros/assert.h"
+
+#include "rosmod/rosmod_common.h"
+#include "ros/types.h"
+#include "rosmod/rosmod_node_handle.h"
+#include "rosmod/rosmod_publisher.h"
+#include "rosmod/rosmod_single_subscriber_publisher.h"
+#include "rosmod/rosmod_service_server.h"
+#include "rosmod/rosmod_subscriber.h"
+#include "rosmod/rosmod_service.h"
+#include "rosmod/rosmod_init.h"
+#include "rosmod/rosmod_master.h"
+#include "rosmod/rosmod_this_node.h"
+#include "rosmod/rosmod_param.h"
+#include "rosmod/rosmod_topic.h"
+#include "rosmod/rosmod_names.h"
+
+#endif
