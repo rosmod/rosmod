1741419c3fdb29c3320224ef89578716f387d87c
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-07-14 13:35:26 -0500

    Testing PFIFO scheduling

--- rosmod/timer-example/src/timer_package/src/timer_package/Timer.cpp
+++ rosmod/timer-example/src/timer_package/src/timer_package/Timer.cpp
@@ -54,7 +54,7 @@ void Timer::startUp()
   std::string advertiseName;
 
   // Scheduling Scheme is FIFO
-  this->compQueue.scheduling_scheme = rosmod::CallbackQueue::SchedulingScheme::FIFO;
+  this->compQueue.scheduling_scheme = rosmod::CallbackQueue::SchedulingScheme::PFIFO;
     
   rosmod::ROSMOD_Callback_Options callback_options;
   callback_options.alias = "Init_Timer";
