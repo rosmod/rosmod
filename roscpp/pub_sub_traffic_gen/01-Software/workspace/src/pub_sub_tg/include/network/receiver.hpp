055248caaae9a8f4977df2e46717ebc080cd97d0
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-08-19 17:47:49 -0500

    updated code to have all network code be in hpp files; when building shared object with templates I had issues getting resolution during loading at run-time.

--- roscpp/pub_sub_traffic_gen/01-Software/workspace/src/pub_sub_tg/include/network/receiver.hpp
+++ roscpp/pub_sub_traffic_gen/01-Software/workspace/src/pub_sub_tg/include/network/receiver.hpp
@@ -1,14 +1,13 @@
 #ifndef NETWORK_RECEIVER_HPP
 #define NETWORK_RECEIVER_HPP
 
+#include <arpa/inet.h>
 #include "ros/ros.h"
 
-#include <arpa/inet.h>
+#include "network/NetworkProfile.hpp"
 #include <boost/thread/thread.hpp>
 
-#include "network/NetworkProfile.hpp"
 #include "network/buffer.hpp"
-#include <std_msgs/UInt64MultiArray.h>
 
 namespace Network
 {
@@ -18,24 +17,165 @@ namespace Network
   class receiver
   {
   public:
-    receiver();
+    receiver()
+    {
+      received_data = false;
 
-    int init(std::string profileName, uint64_t buffer_capacity_bits);
-    void add_sender(std::string profileName);
+      // CREATE THREAD HERE FOR RECEIVING DATA
+      boost::thread *tmr_thread =
+	new boost::thread( boost::bind(&receiver::buffer_recv_threadfunc, this) );
+    }
+
+    int init(std::string profileName, uint64_t buffer_capacity_bits)
+    {
+      profile.initializeFromFile(profileName.c_str());
+      buffer.set_capacityBits(buffer_capacity_bits);
+
+      // NEED TO CREATE MC SOCKET FOR SENDING OOB DATA
+      if ( (oob_mc_send_sockfd = socket(AF_INET, SOCK_DGRAM, 0)) <0)
+	{
+	  perror("socket");
+	  return -1;
+	}
+      memset(&oob_mc_send_sockaddr, 0, sizeof(oob_mc_send_sockaddr));
+      oob_mc_send_sockaddr.sin_family = AF_INET;
+      oob_mc_send_sockaddr.sin_addr.s_addr = inet_addr(oob_mc_group.c_str());
+      oob_mc_send_sockaddr.sin_port = htons(oob_mc_port);
+
+      return 0;
+    }
+
+    void add_sender(std::string profileName)
+    {
+      Network::NetworkProfile p;
+      p.initializeFromFile(profileName.c_str());
+
+      uuids.push_back(p.uuid);
+      profile_map[p.uuid] = p;
+      receive_map[p.uuid] = std::map<ros::Time, uint64_t>();
+    }
 
     void set_duration(double dur) { duration = ros::Duration(dur); }
     void set_output_filename(std::string filename) { output_filename = filename; }
 
-    void update_sender_stream(uint64_t uuid, ros::Time t, uint64_t new_size);
+    void update_sender_stream(uint64_t uuid, ros::Time t, uint64_t new_size)
+    {
+      uint64_t prevData = 0;
+      if ( receive_map[uuid].size() )
+	prevData = receive_map[uuid].end()->second;
+      receive_map[uuid][t] = new_size + prevData;
+    }
 
     ros::Time get_end_time() const { return ros::Time(endTime); }
 
-    int oob_send(std::vector<uint64_t>& send_uuids, bool val);
+    int oob_send(std::vector<uint64_t>& send_uuids, bool val)
+    {
+      int num_disabled = send_uuids.size();
 
-    void buffer_recv_threadfunc(void);
+      // FORMAT DATA
+      char msg[(num_disabled+2) * 16];
+      memset(msg,0,(num_disabled+2)*16);
+      for (auto it = send_uuids.begin(); it != send_uuids.end(); ++it)
+	{
+	  sprintf(msg,"%s%lu,%d;", msg, *it, (int)val);
+	}
 
-    int unlimit_ddos(void);
-    int limit_ddos(ros::Time now, double timeWindow);
+      // SEND FORMATTED DATA
+      if ( (sendto( oob_mc_send_sockfd,
+		    msg,
+		    strlen(msg),
+		    0,
+		    (struct sockaddr*)&oob_mc_send_sockaddr,
+		    sizeof(oob_mc_send_sockaddr) )
+	    ) < 0 )
+	{
+	  perror("sendto");
+	  return -1;
+	}
+      printf("send data to %lu senders!\n",send_uuids.size());
+      return 0;
+    }
+
+    void buffer_recv_threadfunc(void)
+    {
+      while ( true )
+	{
+	  // UPDATE SENDERS IF BUFFER IS CLEARING:
+	  uint64_t currentSize = buffer.bits();
+	  uint64_t currentCapacity = buffer.capacityBits();
+	  if ( currentCapacity > 0 )
+	    {
+	      double utilization = (double)currentSize/(double)currentCapacity;
+	      if ( utilization < 0.75 )
+		unlimit_ddos();
+	    }
+	  // READ FROM THE BUFFER
+	  double timerDelay = -1;
+	  try
+	    {
+	      Network::Message msg = buffer.receive(1000);
+	      if (!received_data)
+		{
+		  ros::Time now = ros::Time::now();
+		  endTime = now + duration;
+		  received_data = true;
+		}
+	      msg.TimeStamp();
+	      messages.push_back(msg);
+	      // CHECK AGAINST RECEIVER PROFILE: LOOK UP WHEN I CAN READ NEXT
+	      timerDelay = profile.Delay(msg.Bits(), msg.LastEpochTime());
+	    }
+	  catch ( Network::Buffer_Empty& ex )
+	    {
+	    }
+	  if ( ros::Time::now() >= endTime )
+	    {
+	      Network::write_data(output_filename.c_str(),messages);
+	      break;
+	    }
+	  else
+	    {
+	      // SLEEP UNTIL NEXT TIME BUFFER CAN BE SERVICED
+	      if (timerDelay > 0)
+		ros::Duration(timerDelay).sleep();
+	    }
+	}
+    }
+
+    int unlimit_ddos(void)
+    {
+      if ( oob_send(disabled_uuids, false) < 0 )
+	return -1;
+      disabled_uuids.clear();
+      return 0;
+    }
+    int limit_ddos(ros::Time now, double timeWindow)
+    {
+      std::vector<uint64_t> bad_uuids;
+      ros::Time prevTime = now - ros::Duration(timeWindow);
+      for (auto uuid_it = uuids.begin();
+	   uuid_it != uuids.end(); ++uuid_it)
+	{
+	  uint64_t d1 = receive_map[*uuid_it].lower_bound(prevTime)->second;
+	  ros::Time t1 = receive_map[*uuid_it].lower_bound(prevTime)->first;
+	  uint64_t d2 = receive_map[*uuid_it].end()->second;
+	  ros::Time t2 = receive_map[*uuid_it].end()->first;
+	  // get data @ t1 from profile, get data @ t2 from profile
+	  uint64_t pd1 = profile_map[*uuid_it].getDataAtTime({t1.sec, t1.nsec});
+	  uint64_t pd2 = profile_map[*uuid_it].getDataAtTime({t2.sec, t2.nsec});
+	  // if profile difference < (d2-d1) : they are sending too much
+	  uint64_t pDiff = pd2 - pd1;
+	  uint64_t diff = d2 - d1;
+	  if (diff > pDiff) {
+	    bad_uuids.push_back(*uuid_it);
+	    if ( std::find(disabled_uuids.begin(),
+			   disabled_uuids.end(),
+			   *uuid_it) == disabled_uuids.end() )
+	      disabled_uuids.push_back(*uuid_it);
+	  }
+	}
+      return oob_send(bad_uuids, true);
+    }
 
   public:
     message_buffer<Network::Message> buffer;
--- roscpp/pub_sub_traffic_gen/01-Software/workspace/src/pub_sub_tg/include/network/sender.hpp
+++ roscpp/pub_sub_traffic_gen/01-Software/workspace/src/pub_sub_tg/include/network/sender.hpp
@@ -6,7 +6,6 @@
 
 #include "network/NetworkProfile.hpp"
 #include <boost/thread/thread.hpp>
-#include <std_msgs/UInt64MultiArray.h>
 
 namespace Network
 {
@@ -18,8 +17,60 @@ namespace Network
   class sender
   {
   public:
-    sender();
-    int init(std::string profileName);
+    sender() { deactivated = false; id = 0; }
+    int init(std::string profileName)
+    {
+      profile.initializeFromFile(profileName.c_str());
+      uuid = profile.uuid;
+
+      // create the multicast receive socket
+      if ( (oob_mc_recv_sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+	  perror("socket");
+	  return -1;
+	}
+      memset(&oob_mc_recv_sockaddr, 0, sizeof(oob_mc_recv_sockaddr));
+      oob_mc_recv_sockaddr.sin_family = AF_INET;
+      oob_mc_recv_sockaddr.sin_port = htons(oob_mc_port);
+      oob_mc_recv_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+      // bind to the addrress
+      if ( bind( oob_mc_recv_sockfd,
+		 (struct sockaddr *)&oob_mc_recv_sockaddr,
+		 sizeof(oob_mc_recv_sockaddr)) )
+	{
+	  perror("binding");
+	  return -1;
+	}
+      // joine the multicast group
+      struct ip_mreq group;
+      group.imr_multiaddr.s_addr = inet_addr(oob_mc_group.c_str());
+      group.imr_interface.s_addr = INADDR_ANY;
+      if ( setsockopt( oob_mc_recv_sockfd,
+		       IPPROTO_IP,
+		       IP_ADD_MEMBERSHIP,
+		       (char *)&group,
+		       sizeof(group) ) < 0 )
+	{
+	  perror("multicast group");
+	  return -1;
+	}
+      // set the receive timeout
+      struct timespec tv;
+      tv.tv_sec = 1;
+      tv.tv_nsec = 0;
+      if ( setsockopt( oob_mc_recv_sockfd,
+		       SOL_SOCKET,
+		       SO_RCVTIMEO,
+		       &tv,
+		       sizeof(tv) ) < 0 )
+	{
+	  perror("timeout setting");
+	  return -1;
+	}
+      // CREATE THREAD FOR RECEIVING OOB COMMUNICATIONS
+      boost::thread oob_recv_thread = boost::thread(&sender::oob_recv_threadfunc, this);
+      return 0;
+    }
 
     void set_duration(double dur) { endTime = ros::Time::now() + ros::Duration(dur); }
     void set_output_filename(std::string filename) { output_filename = filename; }
@@ -27,11 +78,102 @@ namespace Network
     uint64_t get_uuid() const { return uuid; }
     ros::Time get_end_time() const { return ros::Time(endTime); }
 
-    void record();
+    void record() { Network::write_data(output_filename.c_str(), messages); }
 
     template <typename T>
-    double send(ros::Publisher pub, const T& msg);
-    void oob_recv_threadfunc();
+    double send(ros::Publisher pub, const T& msg)
+    {
+      double timeDiff = 0;
+      // CHECK AGAINST PRIVATE VARIABLE : DEACTIVATED
+      if (deactivated)
+	throw Network::Exceeded_Production_Profile();
+      // CHECK AGAINST PROFILE (INCLUDING METERING FROM RECEIVER)
+      if (profile.Initialized()) // profile is not in error state
+	{
+	  uint64_t msgSizeBytes =
+	    ros::serialization::Serializer<T>::serializedLength(msg);
+	  // take a measurement
+	  Network::Message new_msg;
+	  new_msg.Bytes(msgSizeBytes);
+	  new_msg.Id(id);
+	  new_msg.TimeStamp();
+
+	  ros::Time now = ros::Time::now();
+	  timeDiff = (now - nextSendTime).toSec();
+	  if (timeDiff < 0)
+	    {
+	      throw Network::Exceeded_Production_Profile();
+	    }
+      
+	  // record the measurement
+	  messages.push_back(new_msg);
+	  id++;
+
+	  if (profile.resources.size() > 0) // has entries in profile
+	    {
+	      timespec current_time;
+	      current_time.tv_sec = now.sec;
+	      current_time.tv_nsec = now.nsec;
+	      timeDiff = profile.Delay(msgSizeBytes * 8, current_time);
+	      nextSendTime = now + ros::Duration(timeDiff);
+	    }
+	}
+      // IF EVERYTHING IS ALRIGHT, PASS IT THROUGH
+      pub.publish(msg);
+      return timeDiff;
+    }
+    void oob_recv_threadfunc()
+    {
+      while (1)
+	{
+	  // receive udp multicast message here
+	  int status;
+	  char buffer[max_recv_buffer_size];
+
+	  struct sockaddr_in saddr;
+	  int socklen;
+	  memset(&saddr, 0, sizeof(saddr));
+
+	  status = recvfrom( oob_mc_recv_sockfd,
+			     buffer,
+			     max_recv_buffer_size,
+			     0,
+			     (struct sockaddr *)&saddr,
+			     (socklen_t *)&socklen );
+	  if ( status > 0 )
+	    {
+	      printf("got message!\n");
+	      bool has_uuid = false;
+	      bool my_val = false;
+	      // parse if for a list of UUIDs and their deactivated setting
+	      std::string s = buffer;
+	      std::stringstream iss(s);
+	      std::string substr;
+	      while ( std::getline(iss, substr, ';') )
+		{
+		  std::stringstream t_iss(substr);
+		  std::string u;
+		  std::string val;
+		  std::getline(t_iss, u, ',');
+		  std::getline(t_iss, val);
+		  uint64_t tmp_uuid = (uint64_t) atoi(u.c_str());
+		  bool tmp_val = (bool) atoi(val.c_str());
+		  if (tmp_uuid == uuid)
+		    {
+		      has_uuid = true;
+		      my_val = tmp_val;
+		      break;
+		    }
+		}
+	      // if our uuid is in the list, set our deactivated variable accordingly
+	      if ( has_uuid )
+		{
+		  printf("got val: %d\n",my_val);
+		  deactivated = my_val;
+		}
+	    }
+	}
+    }
 
   public:
     std::vector<Network::Message> messages;
