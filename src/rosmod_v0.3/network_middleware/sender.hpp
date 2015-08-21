69aae9cd7fa8d0b3cbdfcfaf7e34312b96f7beee
William Emfinger <waemfinger@gmail.com>
2015-08-21 11:29:13 -0500

    working on updating the templates for generating ddos and traffic generation code into pub/sub components.

--- src/rosmod_v0.3/network_middleware/sender.hpp
+++ src/rosmod_v0.3/network_middleware/sender.hpp
@@ -97,6 +97,7 @@ namespace Network
 
     void set_duration(double dur) { endTime = ros::Time::now() + ros::Duration(dur); }
     void set_output_filename(std::string filename) { output_filename = filename; }
+    void set_uuid( uint64_t u ) { uuid = u; }
 
     uint64_t get_uuid() const { return uuid; }
     ros::Time get_end_time() const { return ros::Time(endTime); }
